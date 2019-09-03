e168db743ab0ad54a5276a2f7213bddbf3811932
peccu <peccul@gmail.com>
2019-09-03 11:43:07 +0900

    remove user specification

--- lumen.sh
+++ lumen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 cd $SCRIPT_DIR
-CURRENT_UID=$(id -u):$(id -g) docker-compose run --rm lumen "$@"
+docker-compose run --rm lumen "$@"
